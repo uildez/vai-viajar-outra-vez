e7199409fb86233f34308aa258914dd90aa8a200
uildez <uildesdacosta@gmail.com>
2025-03-14 11:17:17 -0300

    feat: adding cupons popup and copy promo code

--- app/cupons/page.jsx
+++ app/cupons/page.jsx
@@ -4,18 +4,38 @@ import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import Navbar from '../containers/navbar'
 import Footer from '../containers/footer'
+import { MdModeOfTravel } from "react-icons/md";
 
 export default function Page() {
     const [cupons, setCupons] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [alert, setAlert] = useState(false);
 
     useEffect(() => {
-        fetch("https://script.google.com/macros/s/AKfycbxWMEs7RIZtI8i8Qoj4gbfReMQ9u9yP_vTndwlUGcUc2ZJs7dDxy1bUbdn_3DZQXczfZw/exec")
+        fetch("https://script.google.com/macros/s/AKfycbzt9T8UKY3BVxdSV_Lq37BQgp4fDY92fLpNFTMT9eBDJ8T0FbfH17N-kG_D3nTZzZjj1Q/exec")
             .then(response => response.json())
             .then(data => setCupons(data))
             .catch(error => console.error("Erro ao buscar cupons:", error));
         setLoading(false)
     }, []);
+
+    const handleCopyAndRedirect = (codigo, link) => {
+        navigator.clipboard.writeText(codigo)
+            .then(() => {
+                setAlert(true);
+
+                setTimeout(() => {
+                    setAlert(false);
+                }, 5000);
+
+                setTimeout(() => {
+                    window.open(link, "_blank");
+                }, 3000);
+            })
+            .catch((err) => {
+                console.error("Erro ao copiar:", err);
+            });
+    };
 
     return (
         <div className="font-redonda overflow-hidden max-w-screen min-h-screen">
@@ -43,11 +63,12 @@ export default function Page() {
                                         <h3 className='text-2xl text-white font-extrabold font-sans uppercase'>{cupom.titulo}</h3>
                                         <p className='text-xl text-white font-redonda'>{cupom.descricao}</p>
                                     </div>
-                                    <Link href={cupom.link}
+                                    <button
+                                        onClick={() => handleCopyAndRedirect(cupom.codigo, cupom.link)}
                                         className='flex items-center w-full justify-center gap-4 px-6 py-2 mt-4 bg-yellow-600 text-black rounded-[10px] uppercase font-bold hover:rotate-2 hover:-translate-y-2 transition-transform duration-500'
                                     >
-                                        Aproveitar cupom
-                                    </Link>
+                                        Copiar Cupom e ir para página
+                                    </button>
                                 </div>
                             </div>
                         ))}
@@ -61,6 +82,21 @@ export default function Page() {
                     Voltar para o Início
                 </Link>
             </div>
+
+            {alert &&
+                <motion.div
+                    initial={{ y: 50, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: 50, opacity: 0 }}
+                    transition={{ duration: 0.5, ease: "easeOut" }} className='fixed flex bottom-4 p-4 gap-8 rounded-xl shadow-2xl right-4 bg-blue-600 text-white'
+                >
+                    <MdModeOfTravel className='text-5xl' />
+                    <div className='flex flex-col gap-2'>
+                        <h2 className='font-bold font-akina uppercase'>Código copiado</h2>
+                        <p className='text-sm'>Você será redirecionado para o site!</p>
+                    </div>
+                </motion.div>
+            }
             <Footer />
         </div>
     )
